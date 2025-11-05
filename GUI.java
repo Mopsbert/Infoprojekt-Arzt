c15c0a91b2272eac26554060b150f832dc608e39
Raphael Bertges <bertgesraphael@gmail.com>
2025-11-05 11:58:42 +0100

    Startet labels in GUI

--- GUI.java
+++ GUI.java
@@ -4,11 +4,17 @@ import java.awt.Color;
 import java.awt.Color.*;
 public class GUI
 {
-    /**
-     * Konstruktor für Objekte der Klasse GUI
-     */
+    private View window;
+    private Textfield tfName,tfDescription,tfGender,tfBloodType,tfAge,tfOrganDonor,tfDnr;
+    private Button btnSubmit;
+    
     public GUI()
     {
-        
+        window = new View(1280,720,"Arzpraxis");
+        window.setBackgroundColor(java.awt.Color.BLACK);
+        tfName = new Textfield(60,40,200,30,"Name",window);
+        tfName.setActivated(true);
+        tfDescription = new Textfield(300,40,300,30,"Beschreibung Krankheit",window);
+        tfGender = new Textfield(640,40,160,30,"Geschlecht(m/w/d)",window);
     }
 }
