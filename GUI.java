c536153d79fd030b28e95d012950d496fbe4ff2c
Raphael Bertges <bertgesraphael@gmail.com>
2025-11-05 11:28:58 +0100

    Added Class GUI

---
+++ GUI.java
@@ -0,0 +1,14 @@
+import sas.*;
+import sasio.*;
+import java.awt.Color;
+import java.awt.Color.*;
+public class GUI
+{
+    /**
+     * Konstruktor für Objekte der Klasse GUI
+     */
+    public GUI()
+    {
+        
+    }
+}
