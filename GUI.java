b67ecfa1a14fc8fabac44522e15ca562e72335f3
Raphael Bertges <bertgesraphael@gmail.com>
2025-11-07 12:26:10 +0100

    Finishied Input in GUI & removed BeispielGUI

--- GUI.java
+++ GUI.java
@@ -5,7 +5,7 @@ import java.awt.Color.*;
 public class GUI
 {
     private View window;
-    private Textfield tfName,tfDescription,tfGender,tfBloodType,tfAge,tfOrganDonor,tfDnr;
+    private Textfield tfName,tfDescription,tfGender,tfBloodType,tfAge,tfOrganDonor,tfDnr,tfUrgency;
     private Button btnSubmit;
     
     public GUI()
@@ -15,6 +15,10 @@ public class GUI
         tfName = new Textfield(60,40,200,30,"Name",window);
         tfName.setActivated(true);
         tfDescription = new Textfield(300,40,300,30,"Beschreibung Krankheit",window);
-        tfGender = new Textfield(640,40,160,30,"Geschlecht(m/w/d)",window);
+        tfGender = new Textfield(640,40,200,30,"Geschlecht(m/w/d)",window);
+        tfAge = new Textfield(900,40,100,30,"Alter",window);
+        tfOrganDonor = new Textfield(400,100,200,30,"Organspender (j/n)",window);
+        tfDnr = new Textfield(640,100,200,30,"CPR/DNR (j/n)",window);
+        tfUrgency = new Textfield(900,100,200,30,"Dringlich (j/n)",window);
     }
 }
